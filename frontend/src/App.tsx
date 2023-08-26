399fe73c8f33ee8234830cbc5fba677ebca63ae7
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2023-08-26 10:39:38 -0300

    updating window title dynamically

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import { useEffect } from 'react';
 import './App.css';
 import { LoadingContextProvider } from './contexts/LoadingContext';
 import Routes from './routes';
 
 function App() {
+    useEffect(() => {document.title = process.env.REACT_APP_PROJECT_NAME || document.title}, [])
     return (
         <div className="App">
             <LoadingContextProvider>
