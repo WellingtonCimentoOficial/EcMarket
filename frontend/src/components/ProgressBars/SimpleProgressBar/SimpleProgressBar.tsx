36186de0d10732832a7078dbe98b18efc11ab35c
Wellington Cimento <wellingtoncimentooficial@gmail.com>
2023-11-06 02:26:23 -0300

    progress bar component created

--- frontend/src/components/ProgressBars/SimpleProgressBar/SimpleProgressBar.tsx
+++ frontend/src/components/ProgressBars/SimpleProgressBar/SimpleProgressBar.tsx
@@ -6,9 +6,10 @@ type Props = {
     totalValue: number,
     height?: number,
     backgroundColor?: string,
+    label?: boolean
 }
 
-const SimpleProgressBar = ({ currentValue, totalValue, height, backgroundColor }: Props) => {
+const SimpleProgressBar = ({ currentValue, totalValue, height, backgroundColor, label }: Props) => {
     const [percentage, setPercentage] = useState<number>(0)
 
     useEffect(() => {
@@ -18,6 +19,9 @@ const SimpleProgressBar = ({ currentValue, totalValue, height, backgroundColor }
     return (
         <div className={styles.wrapper}>
             <div className={`${styles.container} ${styles.default}`} style={{height: `${height}px` || '10px'}}></div>
+            <div className={`${styles.container3} ${styles.default}`} style={{height: `${height}px` || '10px', display: label ? 'flex' : 'none'}}>
+                <span className={styles.label} style={{fontSize: height ? `${height - 2}px` : '10px'}}>{Math.floor(percentage)}%</span>
+            </div>
             <div 
                 className={`${styles.container2} ${styles.default}`} 
                 style={{height: `${height}px` || '10px', width: `${percentage}%`, backgroundColor: backgroundColor || 'var(--primary-color)'}}
